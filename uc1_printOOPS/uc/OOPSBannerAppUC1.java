d087c13dbc96083b211361fc0224a679a2320b3d
hm7056-pixel <hm7056@srmist.edu.in>
2026-02-24 10:47:53 +0530

    display oops

---
+++ uc1_printOOPS/uc/OOPSBannerAppUC1.java
@@ -0,0 +1,18 @@
+/**
+ * OOPSBannerApp - UC1
+ * Goal: Display the literal text "OOPS" once on the console.
+ * 
+ * @author YourName
+ * @version 1.0
+ */
+public class OOPSBannerAppUC1 {
+
+    /**
+     * Entry point of the application.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Print OOPS to the console
+        System.out.println("OOPS");
+    }
+}
