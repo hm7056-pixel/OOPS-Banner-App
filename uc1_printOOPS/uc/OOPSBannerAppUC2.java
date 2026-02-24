f15ed02e5158d8a3d18f4435cc317abe8e02662e
hm7056-pixel <hm7056@srmist.edu.in>
2026-02-24 10:56:21 +0530

    uc2 display oppps banner

---
+++ uc1_printOOPS/uc/OOPSBannerAppUC2.java
@@ -0,0 +1,32 @@
+/**
+ * OOPSBannerApp - UC2
+ * Goal: Display "OOPS" in a banner format using * and spaces.
+ * 
+ * @author YourName
+ * @version 2.0
+ */
+public class OOPSBannerAppUC2 {
+
+    /**
+     * Entry point of the application.
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+        // Print OOPS in Banner Format (7 lines, width 9)
+        
+        // Line 1
+        System.out.println(" ***   ***   *****   ***** ");
+        // Line 2
+        System.out.println("*   * *   *  *    *  *     ");
+        // Line 3
+        System.out.println("*   * *   *  *    *  *     ");
+        // Line 4
+        System.out.println("*   * *   *  *****   ***** ");
+        // Line 5
+        System.out.println("*   * *   *  *        *    ");
+        // Line 6
+        System.out.println("*   * *   *  *        *    ");
+        // Line 7
+        System.out.println(" ***   ***   *        ***** ");
+    }
+}
